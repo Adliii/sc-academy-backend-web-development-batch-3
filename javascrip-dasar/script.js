e7e53fb665cbdd230cc6084a0762b6a0253a32a0
Adliii <adlinursah@gmail.com>
2024-09-14 15:44:14 +0700

    first commit

---
+++ javascrip-dasar/script.js
@@ -0,0 +1,47 @@
+// // variabel 
+
+// // variabel tipe data string
+// const variableSatu = "ini variabel";
+// console.log(variableSatu);
+
+// // variabel tipe data integer
+// const variableDua = 10;
+// console.log(variableDua);
+
+// // variabel tipe data desimal
+// const variabelTiga = 3.14;
+// console.log(variabelTiga);
+
+// // variabel tipe data boolean
+// const variabelEmpat = false;
+// console.log(variabelEmpat);
+
+// function
+let data1 = false;
+let data2 = false;
+function coba(data1,data2){
+    // console.log(data1);
+    // console.log(data2);
+    // return data1;    
+    if(data1 === true){
+        data1 = 'berhasil login';
+    }else {
+        data1 = 'gagal login';
+    }
+    console.log(data1);
+
+
+}
+coba (data1,data2);
+
+
+nilai1 = 10;
+nilai2 = 10;
+// perkalian
+function perkalian (nilai1,nilai2){
+    const hasil = nilai1*nilai2;
+    console.log(hasil);
+}
+
+perkalian(nilai1,nilai2);
+console.log()
